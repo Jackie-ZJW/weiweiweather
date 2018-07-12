4b75eb7e623780b9e43db0d21e178293d05e86b4
ZJW19890602 <1192935956@qq.com>
2018-07-13 01:25:17 +0800

    配置百度地图定位SDK相关

---
+++ app/src/main/java/com/example/zhangjianwei/weiweiweather/WelcomeActivity.java
@@ -0,0 +1,153 @@
+package com.example.zhangjianwei.weiweiweather;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient;
+
+    private MyLocationListener locationListener;
+
+    private ProgressDialog mProgressDialog;
+
+    private String address;
+
+    private String province;
+
+    private String city;
+
+    private String county;
+
+    private String street;
+
+    private Boolean isAuthorized = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        initData();
+
+    }
+
+    private void initData() {
+
+
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(WelcomeActivity.this, permissions, 1);
+        } else {
+
+            initBaiduMapLocation();
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(WelcomeActivity.this, "必须同意所有权限才能正常使用本程序！", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+
+                    initBaiduMapLocation();
+                } else {
+                    Toast.makeText(WelcomeActivity.this, "发生了未知错误！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initBaiduMapLocation() {
+        showProgressDialog("开始定位...");
+        mLocationClient = new LocationClient(this.getApplicationContext());
+        locationListener=new MyLocationListener();
+        mLocationClient.registerLocationListener(locationListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+
+
+    }
+
+    private void showProgressDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(message);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(false);
+        }
+
+        mProgressDialog.show();
+
+    }
+
+    private void cancelProgressDialog() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            address = bdLocation.getAddrStr();
+            province = bdLocation.getProvince();
+            city = bdLocation.getCity();
+            county = bdLocation.getDistrict();
+            street = bdLocation.getStreet();
+            if (!TextUtils.isEmpty(address)) {
+                Log.e("zhangjianwei1234", address);
+                cancelProgressDialog();
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "获取定位信息失败！", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
